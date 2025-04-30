972863463b3ef528a7b462d1c40d724c57e555e6
Soromytko <NikitaSoromytko@gmail.com>
2025-05-01 00:18:53 +0300

    Add more scissor sounds #3

--- PaperCutProto/Assets/Scripts/ScissorsSoundPlayer.cs
+++ PaperCutProto/Assets/Scripts/ScissorsSoundPlayer.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class ScissorsSoundPlayer : MonoBehaviour
 {
+    [SerializeField] private AudioClip[] _audioClips;
     private AudioSource _audioSource;
 
     private void Awake()
@@ -14,6 +15,8 @@ public class ScissorsSoundPlayer : MonoBehaviour
     {
         if (!_audioSource.isPlaying)
         {
+            int clipIndex = Random.Range(0, _audioClips.Length);
+            _audioSource.clip = _audioClips[clipIndex];
             _audioSource.Play();
         }
     }
