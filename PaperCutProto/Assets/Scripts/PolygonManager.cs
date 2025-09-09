255b2e2104eb5ec0aa3ee24143fa2d6e0ae6f499
Soromytko <NikitaSoromytko@gmail.com>
2025-09-09 14:13:16 +0300

    Add hole cutting feature

--- PaperCutProto/Assets/Scripts/PolygonManager.cs
+++ PaperCutProto/Assets/Scripts/PolygonManager.cs
@@ -10,10 +10,12 @@ public class PolygonManager : MonoBehaviour
     public Polygon TargetPolygon => _targetPolygon;
 
     [SerializeField] private Polygon _polygonPrefab;
+    [SerializeField] private Polygon _holePolygonPrefab;
     [SerializeField] private Polygon _mainPolygon;
     [SerializeField] private Polygon _targetPolygon;
 
     private List<Polygon> _polygons = new List<Polygon>();
+    private List<Polygon> _holePolygons = new List<Polygon>();
 
     private void Awake()
     {
@@ -43,6 +45,15 @@ public class PolygonManager : MonoBehaviour
         result.Shape.SetPoints(points);
         result.transform.parent = transform;
         _polygons.Add(result);
+        return result;
+    }
+
+    public Polygon CreateHolePolygon(Vector2[] points)
+    {
+        Polygon result = Instantiate(_holePolygonPrefab);
+        result.Shape.SetPoints(points.ToList());
+        result.transform.parent = transform;
+        _holePolygons.Add(result);
         return result;
     }
 
